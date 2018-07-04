03d181f04fae4148eba3a68cdc703e067ae055a0
Big Mogician <sunaoran@youzan.com>
2018-07-04 15:02:13 +0800

    add : pretty option

--- src/plugins/dev/ts-node.js
+++ src/plugins/dev/ts-node.js
@@ -1,5 +1,6 @@
 const { TS_CONFIG } = process.env;
 
-const tsnode = require("ts-node").register(TS_CONFIG && {
-  project: TS_CONFIG
+const tsnode = require("ts-node").register({
+  project: TS_CONFIG,
+  pretty: true
 })
